4316dbf9cd83cfeeaf0fa73bc0d4469869baacf0
misterhamy <max.homenko01@gmail.com>
2023-03-21 16:15:25 +0200

    2,3uzd

---
+++ 2,3uzdevums/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _2_3uzdevums
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labDatums = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labDatums
+            // 
+            this.labDatums.AutoSize = true;
+            this.labDatums.Location = new System.Drawing.Point(29, 95);
+            this.labDatums.Name = "labDatums";
+            this.labDatums.Size = new System.Drawing.Size(35, 13);
+            this.labDatums.TabIndex = 0;
+            this.labDatums.Text = "label1";
+            this.labDatums.Click += new System.EventHandler(this.labDatums_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labDatums);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labDatums;
+    }
+}
+
---
+++ 2,3uzdevums/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Main(args, text2);
+    }
+
+    static void Main(string[] args, DateTime text2)
+    {
+        DateTime theDate;
+
+        string file = "C:\Users\maxho\OneDrive\Desktop\repozitorijas\programmesana-3\2,3uzdevums\2,3.uzd.txt";
+
+        string text1 = "Datumi kad programma 2,3uzdevums bija iedarbināta:"; 
+        File.WriteAllText(file, text1);
+
+        var text2 = (theDate = DateTime.Now);
+        File.AppendAllText(file, text2);
+
+        Console.WriteLine(File.ReadAllText(file));
+        Console.ReadKey();
+    }
+}
+
+namespace _2_3uzdevums
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime theDate;
+
+            theDate = DateTime.Now;
+
+            labDatums.Text = (theDate.ToString());
+
+            string writeText = (theDate.ToString());
+
+        }
+
+
+
+        private void labDatums_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
